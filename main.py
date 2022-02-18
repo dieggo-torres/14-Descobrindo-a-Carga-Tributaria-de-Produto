64037e6258913743509ad34a6bb5dafd4791144d
Diego Moura Torres <95168133+dieggo-torres@users.noreply.github.com>
2022-02-18 09:47:33 -0300

    Criar módulo para calcular carga tributária sobre preço final de produto.

---
+++ main.py
@@ -0,0 +1,26 @@
+# Dados do produto
+preco_final = 1500
+custo_unitario = 400
+lucro = 800
+
+
+def calcular_carga_tributaria(preco, custo, lucro):
+    """Calcula a carga tributária que incide sobre um produto
+
+    Parâmetros:
+        preco (float): preço final do produto
+        custo (float): custo unitário do produto
+        lucro (float): lucro obtido com a venda de uma unidade do produto
+
+    Retorno:
+        carga_tributaria (float): carga tributária que incide sobre o produto
+    """
+    carga_tributaria = (preco - custo - lucro) / preco
+    return carga_tributaria
+
+
+percentual_carga_tributaria = calcular_carga_tributaria(
+    preco_final, custo_unitario, lucro)
+percentual_carga_tributaria = "{:.2%}".format(
+    percentual_carga_tributaria).replace(".", ",")
+print("Carga tributária: {}".format(percentual_carga_tributaria))
